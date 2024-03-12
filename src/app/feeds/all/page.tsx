305d0196760ffd8aa1feddf03341503928282c8e
busyrat <1435858847@qq.com>
2024-03-12 17:20:50 +0800

    feat: 使用连表查询

--- src/app/feeds/all/page.tsx
+++ src/app/feeds/all/page.tsx
@@ -12,19 +12,31 @@ export interface Row extends feeds {
 }
 
 export default async function Page() {
+  // const { rows } = await sql<Row>`
+  //   SELECT * FROM feeds
+  // `
+
+  // for (const row of rows) {
+  //   const { rows: mixes } = await sql<files>`
+  //     SELECT * FROM files
+  //     WHERE parent_file_id = ${row.file_id}
+  //     AND read_flag IS NULL
+  //   `
+  //   // console.log(children[0]);
+  //   row.mixes = mixes
+  // }
+
   const { rows } = await sql<Row>`
-    SELECT * FROM feeds
+    SELECT 
+      f.*, 
+      CASE WHEN COUNT(c) = 0 then '[]' ELSE json_agg(c) end AS mixes
+    FROM feeds f
+    LEFT JOIN files c ON c.parent_file_id = f.file_id AND c.read_flag IS NULL  
+    GROUP BY f.file_id, f.name, f.share_id, f.id
   `
 
-  for (const row of rows) {
-    const { rows: mixes } = await sql<files>`
-      SELECT * FROM files
-      WHERE parent_file_id = ${row.file_id}
-      AND read_flag IS NULL
-    `
-    // console.log(children[0]);
-    row.mixes = mixes
-  }
+  // 第三行也可以替换成下面这一句 https://stackoverflow.com/questions/24155190/postgresql-left-join-json-agg-ignore-remove-null
+  // COALESCE(NULLIF(json_agg(c)::TEXT, '[null]'), '[]')::JSON AS mixes
 
   return <>
     <FeedsTable feeds={rows} />
