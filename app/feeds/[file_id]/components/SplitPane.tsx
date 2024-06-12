2101f6e9f0387e82227c21b61d256e5556fcd4bc
busyrat <1435858847@qq.com>
2024-06-12 18:55:43 +0800

    feat: 全文

--- app/feeds/[file_id]/components/SplitPane.tsx
+++ app/feeds/[file_id]/components/SplitPane.tsx
@@ -1,16 +1,45 @@
 'use client'
 
-import React from 'react'
+import React, { useRef } from 'react'
 import { Allotment } from "allotment";
 import "allotment/dist/style.css";
+
+interface SplitPaneComponentRef {
+  resize: (sizes: number[]) => void;
+}
 
 
 const SplitPane = (props: any) => {
   const { children, ...rest } = props;
+  const ref = useRef<SplitPaneComponentRef>();
+  const [size, setSize] = React.useState([300, 1000]);
+
+  const onChange = (...params: any) => {
+    setSize(params[0])
+    if (rest.onChange) {
+      rest.onChange(...params);
+    }
+  }
+
   return (
-    <Allotment { ...rest }>
-      {props.children}
-    </Allotment>
+    <>
+      <button
+        onClick={() => {
+          if (ref.current) {
+            if (size[0] === 0) {
+              ref.current.resize([1000, 0]);
+            } else {
+              ref.current.resize([0, 1000]);
+            }
+          }
+        }}
+      >
+        { size[0] === 0 ? '全文' : '目录'}
+      </button>
+      <Allotment { ...rest } ref={ref} onChange={onChange}>
+        {props.children}
+      </Allotment>
+    </>
   )
 }
 
