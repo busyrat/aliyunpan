e6ee262b2556759fa809945dc5ca72ece9ed06bb
busyrat <1435858847@qq.com>
2024-06-18 19:26:39 +0800

    feat: 引入 shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Button } from "@/components/ui/button"
 import Link from 'next/link'
 
 const Home = () => (
   <main className="flex flex-col justify-center items-center">
     <h1 className="text-4xl mb-10">阿里云盘分享订阅</h1>
     <Link href="/feeds/all">
-      <Button type="primary">feeds</Button>
+      <Button>feeds</Button>
     </Link>
   </main>
 );
