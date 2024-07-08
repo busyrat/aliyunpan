1b3f77ea01b451ddeb79bbaee2aa934ac70435bb
busyrat <1435858847@qq.com>
2024-07-08 17:39:17 +0800

    fix: 图片地址替换

--- app/feeds/[file_id]/[sub_file_id]/components/Markdown.tsx
+++ app/feeds/[file_id]/[sub_file_id]/components/Markdown.tsx
@@ -12,6 +12,15 @@ type Props = {
   file: any
 }
 
+function replaceImageUrl (text: string) {
+  // https://user-gold-cdn.xitu.io/2019/5/13/16ab0c39cfa5125d?imageView2/0/w/1280/h/960/format/webp/ignore-error/1
+  // 替换成
+  // https://p1-jj.byteimg.com/tos-cn-i-t2oaga2asx/gold-user-assets/2019/5/13/16ab0c39cfa5125d~tplv-t2oaga2asx-jj-mark:1512:0:0:0:q75.awebp
+
+  text = text.replaceAll('user-gold-cdn.xitu.io', 'p1-jj.byteimg.com/tos-cn-i-t2oaga2asx/gold-user-assets')
+  text = text.replaceAll('?imageView2/0/w/1280/h/960/format/webp/ignore-error/1', '~tplv-t2oaga2asx-jj-mark:1512:0:0:0:q75.awebp')
+  return text
+}
 
 const MarkdownPreview: React.FC<Props> = async ({ file }) => {
   const resp = await axios.get(file.link.url, {
@@ -24,6 +33,8 @@ const MarkdownPreview: React.FC<Props> = async ({ file }) => {
   // const content = await resp.data.arrayBuffer()
   const textDecoder = new TextDecoder('utf-8')
   let text = textDecoder.decode(resp.data)
+  
+  text = replaceImageUrl(text)
 
   text = `# ${file.name}\n${text}`
 
